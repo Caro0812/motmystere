42264ade3aeed3805aa95be8b39a6974f2e257a5
Caro 0812 <croux.carole@wanadoo.fr>
2024-01-25 15:00:43 +0100

    begin feature/bonus

--- code/php/register.php
+++ code/php/register.php
@@ -120,7 +120,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				</div>
 			</article>
 		</section>
-
+ 
 		 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
